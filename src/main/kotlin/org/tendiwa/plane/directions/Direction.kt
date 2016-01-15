8ba82ac12abaf1a4977ec2311b1910f4a5dbeb93
Georgy Vlasov <wlasowegor@gmail.com>
2016-01-15 18:36:18 +0500

    Replace Corner with Angle

--- src/main/kotlin/org/tendiwa/plane/directions/Direction.kt
+++ src/main/kotlin/org/tendiwa/plane/directions/Direction.kt
@@ -1,6 +1,9 @@
 package org.tendiwa.plane.directions
 
 import org.tendiwa.math.angles.AngularMeasure
+import org.tendiwa.plane.directions.OrdinalDirection.*
+import org.tendiwa.tools.butIf
+import java.lang.Math.PI
 
 /**
  * A direction in two-dimensional space.
@@ -20,11 +23,30 @@ interface Direction {
      * Returns a positive counter-clockwise angle between two vectors
      */
     infix fun counterClockwiseAngle(ccw: Direction): AngularMeasure =
-        if (ccw.radians < this.radians) {
-            val denormalizedRadians = ccw.radians + Math.PI * 2
-            AngularMeasure(denormalizedRadians - this.radians)
-        } else {
-            AngularMeasure(ccw.radians - this.radians)
-        }
+        AngularMeasure(
+            ccw.greaterAlternativeRadians(this) - radians
+        )
 }
 
+fun Direction.greaterAlternativeRadians(cw: Direction): Double =
+    this.radians
+        .butIf(
+            { cw.radians > this.radians },
+            { this.radians + PI * 2 }
+        )
+
+val Direction.quarter: OrdinalDirection?
+    // TODO: Can be optimized
+    get() = if (radians > 0 && radians < Math.PI / 2) {
+        NE
+    } else if (radians > Math.PI / 2 && radians < Math.PI) {
+        NW
+    } else if (radians > Math.PI && radians < Math.PI / 2 * 3) {
+        SW
+    } else if (radians > Math.PI / 2 * 3 && radians < Math.PI * 2) {
+        SE
+    } else {
+        null
+    }
+
+fun Direction.reverse(): Direction = RadianDirection(radians + PI)
